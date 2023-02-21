c4b1e3bab021b222639502f48b988a7851472757
ChetraGithub <111889662+ChetraGithub@users.noreply.github.com>
2023-02-21 13:15:17 +0700

    Add files via upload

---
+++ views/partials/js/nav.js
@@ -0,0 +1,39 @@
+// FUNCTIONS
+function show(element)
+{
+    element.style.display = 'block';
+}
+
+function hide(element)
+{
+    element.style.display = 'none';
+}
+
+function showSitting () {
+    const sitting = $('#sitting')[0];
+    show(sitting);
+}
+
+
+// ADD EVENTS
+const logoutDailog = $('.logout-dailog')[0];
+
+$("#sitting").on( "mouseover", function() {
+    show(this);
+});
+
+$("body").on( "mouseout", function() {
+    hide($("#sitting")[0]);
+});
+
+$("#sitting").on( "mouseout", function() {
+    hide(this);
+});
+
+$("#btn-logout").on( "click", function() {
+    show(logoutDailog);
+});
+
+$("#logout-cancel").on( "click", function() {
+    hide(logoutDailog);
+});
